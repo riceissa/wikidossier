58e0aa6d9db9c6f8b40417a01da79a1ed522abfe
Issa Rice <riceissa@gmail.com>
2017-02-11 13:03:07 -0800

    chop long lines

--- print_page.py
+++ print_page.py
@@ -18,14 +18,28 @@ def main():
     print("  <h1>Wikidossier for {}</h1>".format(username))
     print("  <p>Here is some basic information about this user:</p>")
     print('  <ul>')
-    print('    <li><a href="https://tools.wmflabs.org/xtools-ec/?user={}&amp;project=en.wikipedia.org">Edit count</a></li>'.format(username))
-    print('    <li><a href="https://tools.wmflabs.org/xtools/pages/?user={}&amp;project=en.wikipedia.org&amp;namespace=all&amp;redirects=none">Pages created</a></li>'.format(username))
+    print('    <li><a href="#edit-count">Edit count</a></li>'.format(username))
+    print('    <li><a href="#pages-created">Pages created</a></li>' \
+            .format(username))
     print('    <li><a href="https://tools.wmflabs.org/afdstats/afdstats.py?name={}&amp;max=500&amp;startdate=&amp;altname=">AfD votes</a></li>'.format(username))
     print('    <li><a href="https://en.wikipedia.org/wiki/Special:PrefixIndex/User:{}/">Subpages</a></li>'.format(username))
     print('  </ul>')
-    print('''
-    <iframe src="https://tools.wmflabs.org/xtools-ec/?user={}&amp;project=en.wikipedia.org"></iframe>
-            '''.format(username))
+    print(('''
+        <h2 id="edit-count">Edit count</h2>
+        <p><a href="https://tools.wmflabs.org/xtools-ec/?user={u}'''
+        '''&amp;project=en.wikipedia.org">Edit count</a></p>
+        <iframe src="https://tools.wmflabs.org/xtools-ec/?user={u}'''
+        '''&amp;project=en.wikipedia.org"></iframe>
+    ''').format(u=username))
+    print(('''
+        <h2 id="pages-created">Pages created</h2>
+        <p><a href="https://tools.wmflabs.org/xtools/pages/?user={u}'''
+        '''&amp;project=en.wikipedia.org&amp;namespace=all&amp;'''
+        '''redirects=none">Pages created</a></p>
+        <iframe src="https://tools.wmflabs.org/xtools/pages/?user={u}'''
+        '''&amp;project=en.wikipedia.org&amp;namespace=all&amp;'''
+        '''redirects=none"></iframe>
+    ''').format(u=username))
     print("</body>")
 
 if __name__ == "__main__":
