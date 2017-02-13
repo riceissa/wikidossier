12a5957193e8b022b4a50bca8fc8346ad47e8191
Issa Rice <riceissa@gmail.com>
2017-02-13 14:10:22 -0800

    Add namespace strings as col

--- plot.py
+++ plot.py
@@ -8,3 +8,4 @@ import util
 df = pd.read_csv("contribs.tsv", sep="\t")
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 ts_df = df.set_index('timestamp').sort_index()
+ts_df['ns_str'] = ts_df.ns.map(lambda x: util.NAMESPACES_INV_MAP[x])
