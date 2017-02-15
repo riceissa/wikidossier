939c88311ea3b5418137869a9bcec7c2cf926f96
Issa Rice <riceissa@gmail.com>
2017-02-14 19:22:40 -0800

    Derive namespaces list from map keys

--- util.py
+++ util.py
@@ -50,40 +50,7 @@ NAMESPACES_MAP = {
 
 NAMESPACES_INV_MAP = {v: k for k, v in NAMESPACES_MAP.items()}
 
-NAMESPACES = [
-    "Main",
-    "Talk",
-    "User",
-    "User talk",
-    "Wikipedia",
-    "Wikipedia talk",
-    "File",
-    "File talk",
-    "MediaWiki",
-    "MediaWiki talk",
-    "Template",
-    "Template talk",
-    "Help",
-    "Help talk",
-    "Category",
-    "Category talk",
-    "Portal",
-    "Portal talk",
-    "Book",
-    "Book talk",
-    "Draft",
-    "Draft talk",
-    "Education Program",
-    "Education Program talk",
-    "TimedText",
-    "TimedText talk",
-    "Module",
-    "Module talk",
-    "Gadget",
-    "Gadget talk",
-    "Gadget definition",
-    "Gadget definition talk",
-]
+NAMESPACES = list(NAMESPACES_MAP.keys())
 
 # Modified from https://www.mediawiki.org/wiki/API:Query#Continuing_queries
 def query(request, lang="en", sleep=1):
