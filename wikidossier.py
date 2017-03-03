f06be4ed668203cc79877946bfafd0ac91620fa9
Issa Rice <riceissa@gmail.com>
2017-03-03 14:40:02 -0800

    Add flask app

---
+++ wikidossier.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+# Modified from http://stackoverflow.com/a/10181810/3422337
+# See also http://stackoverflow.com/a/14257969/3422337
+
+import time
+from threading import Lock
+from flask import Flask, request
+import sys
+app = Flask(__name__)
+
+a = 1
+b = 2
+c = 3
+lock = Lock()
+
+@app.route("/user", methods=["GET", "POST"])
+def user_front():
+    if request.method == "POST":
+        username = sanitize_username(request.form['username'])
+        if username:
+            return hello(username)
+        else:
+            return "Invalid username"
+    page = """<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+                <title>awesome app</title>
+            </head>
+            <body>
+                <h1>Wikidossier</h1>
+                <p>h-hi oniichan, who are you going to make me stalk
+                    today? ;_;</p>
+                <form method="post">
+                    <input type="text" placeholder="Enter valid English Wikipedia username" name="username" style="width: 400px;" autofocus>
+                    <input type="submit" value="stalk ’em!">
+                </form>
+            </body>
+        </html>
+    """
+    return page
+
+@app.route("/user/<username>")
+def hello(username):
+    # global a
+    global b
+    global c
+    has_data = False
+    if has_data:
+        # The server has the data stored, so just read it
+        pass
+    else:
+        # The server does not have the data for this user, so query the API for
+        # the data
+        with lock:
+            # a += 1
+            print(a, file=sys.stderr)
+            b += a
+            c += b
+            # time.sleep(5) # Simulate long process
+    return "Hello, {}! a={}, b={}, c={}".format(username, a, b, c)
+
+def sanitize_username(username):
+    '''
+    Take an English Wikipedia username. If the username is safe, return a
+    normalized version of the username. Otherwise return None.
+    '''
+    username = str(username)
+    username = username.replace("_", " ")
+    bad_chars = set(map(chr, range(32)))
+    bad_chars = bad_chars.union(set(map(chr, range(127, 161))))
+    bad_chars = bad_chars.union(set(["|", "/", ":"]))
+    if set(username).intersection(bad_chars):
+        # There were bad chars, so don't continue with this username
+        return None
+    return username
+
+if __name__ == "__main__":
+    app.run()
