eef77f33d45ef5ca747a4954dbbcf3042ff4279e
Vipul Naik <vipulnaik1@gmail.com>
2017-07-29 20:29:59 +0000

    Fix to sizediff uclimit

--- sizediff.py
+++ sizediff.py
@@ -33,7 +33,7 @@ def process_user(username):
             'list': 'usercontribs',
             'ucprop': 'sizediff|timestamp|ids|title',
             'ucuser': username,
-            'uclimit': 100,
+            'uclimit': 500,
     }
     for result in util.query(ucpayload, sleep=0):
         for i in result['usercontribs']:
